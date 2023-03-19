c085e18997615472d39af501212845600d65fe30
Vivek Tiwari <tiwarivivek44@gmail.com>
2023-03-20 04:40:23 +0530

    Initial Commit

---
+++ src/app/fake-data.ts
@@ -0,0 +1,28 @@
+import { Listing } from './types';
+
+export const fakeListings: Listing[] = [
+  {
+    id: '123',
+    name: 'Old Boat',
+    description: 'A very old boat. Bargain price',
+    price: 700,
+  },
+  {
+    id: '345',
+    name: 'Computer',
+    description: "From the 1990's. A classic!",
+    price: 50,
+  },
+  {
+    id: '456',
+    name: 'Basket ball',
+    description: 'Good condition. Free delivery',
+    price: 100,
+  },
+  {
+    id: '987',
+    name: 'Play Station',
+    description: 'Old Play Station. Still works fine',
+    price: 200,
+  },
+];
